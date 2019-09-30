06b10719e21aeaddf15675f0f01175071f9d37d0
Christian Clauss <cclauss@me.com>
2019-09-30 22:51:53 +0200

    golangci-lint run

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,17 +2,5 @@
 set -ex
 dep ensure
 go install ./cmd/misspell
-gometalinter \
-  --vendor \
-  --deadline=60s \
-  --disable-all \
-  --enable=vet \
-  --enable=golint \
-  --enable=gofmt \
-  --enable=goimports \
-  --enable=gosimple \
-  --enable=staticcheck \
-  --enable=ineffassign \
-  --exclude=/usr/local/go/src/net/lookup_unix.go \
- ./...
+golangci-lint run
 go test .
