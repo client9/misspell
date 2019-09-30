f2833859f9bb6026174f7642d124a91353ccaa0d
Christian Clauss <cclauss@me.com>
2019-09-30 22:51:04 +0200

    gometalinter --> golangci-lint

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,8 +4,8 @@ set -ex
 # DEP
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
-# GOMETALINTER
-go get -u github.com/alecthomas/gometalinter && gometalinter --install
+# golangci-lint
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 
 # remove the default misspell to make sure
 rm -f `which misspell`
